6243e2401dc15061013e70eb64c0a02c5c948cc4
sodonova <54714602+sodonova@users.noreply.github.com>
2022-03-31 12:41:25 -0400

    Change notes from draft 1

--- wrapup.bat
+++ wrapup.bat
@@ -1,4 +1,6 @@
-echo staging changes and committing %1 to overleaf and github
+echo Compiling
+Rscript -e "rmarkdown::render('Paper.Rmd', 'all')"
+echo Staging changes and committing %1 to overleaf and github
 git add .
 git commit -m %1
 git push origin
